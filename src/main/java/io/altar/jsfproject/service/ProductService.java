b02f7ff61f65819bbd0a4e6074a5ba0fad860aaa
Stiffrain <nunomoreira16@gmail.com>
2017-05-29 01:50:53 +0100

    so falta a shelf!!!!!!!!!!yeyyeyeyeye :D

--- src/main/java/io/altar/jsfproject/service/ProductService.java
+++ src/main/java/io/altar/jsfproject/service/ProductService.java
@@ -1,35 +1,64 @@
 package io.altar.jsfproject.service;
 
-
 import java.util.Collection;
 
-import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import io.altar.jsfproject.model.Product;
 import io.altar.jsfproject.repositories.DB_bean;
+import io.altar.jsfproject.view.ProductBean;
 
 @Named("ProductService")
-@RequestScoped
-public class ProductService{
+@ApplicationScoped
+public class ProductService {
 	
-	public void create(Product product){
-		DB_bean.getPRODUCT_REPOSITORY().create(product);
+	@Inject
+	private ProductBean productBean;
+
+	private boolean editable = false;
+	private long productId;
+
+	public boolean isEditable() {
+		return editable;
+	}
+
+	public void setEditable(boolean editable) {
+		this.editable = editable;
 	}
 	
-	public void update(Product product){
-		DB_bean.getPRODUCT_REPOSITORY().update(product);
+	public void edit(long id){
+		this.editable = true;
+		this.productId = id;
+		productBean.setProduct(this.consult(id));
 	}
-	
+
+	public void toMenu() {
+		if (!isEditable()) { // se nao for editavel é porque tem de ser criado
+			Product product = productBean.getProduct();
+			
+			DB_bean.getPRODUCT_REPOSITORY().create(product);
+			
+		} else if (isEditable()) { // se for editavel faz-se update
+			productBean.getProduct().setId(this.productId);
+			Product product = productBean.getProduct();
+			
+			DB_bean.getPRODUCT_REPOSITORY().update(product);
+			this.editable = false;
+		}
+		// return "product-menu?faces-redirect=true";
+	}
+
 	public void remove(Product product) {
 		DB_bean.getPRODUCT_REPOSITORY().remove(product.getId());
 	}
-	
-	public Collection<Product> consult(){
+
+	public Collection<Product> consult() {
 		return DB_bean.getPRODUCT_REPOSITORY().consult();
 	}
-	
-	public Product consult(long id){
+
+	public Product consult(long id) {
 		return DB_bean.getPRODUCT_REPOSITORY().consult(id);
 	}
 
--- src/main/java/io/altar/jsfproject/view/ProductBean.java
+++ src/main/java/io/altar/jsfproject/view/ProductBean.java
@@ -1,14 +1,10 @@
 package io.altar.jsfproject.view;
 
 
-import java.util.Collection;
-
 import javax.enterprise.context.RequestScoped;
-import javax.inject.Inject;
 import javax.inject.Named;
 
 import io.altar.jsfproject.model.Product;
-import io.altar.jsfproject.service.ProductService;
 
 
 @Named("ProductBean")
@@ -16,48 +12,13 @@ import io.altar.jsfproject.service.ProductService;
 public class ProductBean {
 	
 	private Product product = new Product();
-	private boolean editable = false;
 	
-	@Inject
-	private ProductService productService;
-
-	public void toMenu() {
-		if (!isEditable()) { // se nao for editavel é porque tem de ser criado
-			productService.create(product);
-		} else if (isEditable()) { // se for editavel
-			productService.update(product);
-		}
-//		return "product-menu?faces-redirect=true";
-	}
-	
-	public void remove(){
-		productService.remove(product);
-	}
-	
-	public Collection<Product> consult(){ 
-		return productService.consult();
-	}
-	
-	public void edit(Long id){
-		editable = true;
-		this.product = productService.consult(id);
-//		return "product?faces-redirect=true";
-	}
-
 	public Product getProduct() {
 		return product;
 	}
 
 	public void setProduct(Product product) {
 		this.product = product;
-	}
-
-	public boolean isEditable() {
-		return editable;
-	}
-
-	public void setEditable(boolean editable) {
-		this.editable = editable;
 	}
 	
 }
